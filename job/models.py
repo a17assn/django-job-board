aea90278d40880a65c27f14a8858615685302a81
a17assn <hassanaq98@gmail.com>
2020-09-04 00:02:41 +0200

    add new templets & add authantication

--- job/models.py
+++ job/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,9 +8,14 @@ JOB_TYPE = (
     ("Full Time", "Full Time"),
     ("Part Time", "Part Time"),
 )
+# instance = object upload
+def image_upload(instance, filename):
+    imagename, extenion = filename.split(".")
+    return "jobs/%s.%s" % (instance.id, extenion)
 
 
 class Job(models.Model):
+    owner = models.ForeignKey(User, related_name="job_owner", on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     # location
     jobType = models.CharField(max_length=15, choices=JOB_TYPE)
@@ -18,6 +25,12 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    img = models.ImageField(upload_to=image_upload)
+    slug = models.SlugField(blank=True, null=True)
+    
+    def save(self, *args, **kwargs):
+       self.slug = slugify(self.title)
+       super(Job, self).save(*args, **kwargs) # Call the real save() method
 
     def __str__(self):
         return self.title
@@ -28,3 +41,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Apply(models.Model):
+    job = models.ForeignKey(Job, related_name='apply_job', on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=100)
+    website = models.URLField(max_length = 200)
+    cv = models.FileField( upload_to='apply/')
+    cover_letter = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    
+
+    def __str__(self):
+        return self.name
+
