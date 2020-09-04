efae7c40ea44ac265b05a0d42aee4fb4e7e72069
a17assn <hassanaq98@gmail.com>
2020-09-05 00:05:11 +0200

    add contact app & signup profileuser

--- accounts/models.py
+++ accounts/models.py
@@ -9,15 +9,28 @@ from django.core.validators import RegexValidator
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    city = models.ForeignKey('City', related_name='user_city', on_delete=models.CASCADE ,  blank=True, null=True)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
-    img = models.ImageField(upload_to='profile/')
+    city = models.ForeignKey(
+        "City",
+        related_name="user_city",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    phone_regex = RegexValidator(
+        regex=r"^\+?1?\d{9,15}$",
+        message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True
+    )  # validators should be a list
+    img = models.ImageField(upload_to="profile/")
 
     def __str__(self):
-      return str(self.user)
+        return str(self.user)
+
 
 ### create new user ---> create new empty profile
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -26,5 +39,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 
 class City(models.Model):
-    name = models.CharField(max_length = 30)
-    
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls',namespace='accounts')),
     path("admin/", admin.site.urls),
     path("jobs/", include("job.urls", namespace="jobs")),
+    path("contact-us/", include("contact.urls", namespace="contact")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
